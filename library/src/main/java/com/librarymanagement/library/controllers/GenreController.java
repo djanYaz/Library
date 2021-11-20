7d6c9a876e4f93d2d9d4d2fa50f719f12bf0c2c9
daianadimitrova258 <daiana_00@abv.bg>
2021-11-20 17:30:33 +0200

    toLowerCase check for genre existence

--- library/src/main/java/com/librarymanagement/library/controllers/GenreController.java
+++ library/src/main/java/com/librarymanagement/library/controllers/GenreController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 @CrossOrigin(origins = "*" , maxAge = 3600)
 @RestController
@@ -28,9 +29,9 @@ public class GenreController {
 
     @PostMapping("/newgenre")
     public String addGenre(@RequestParam(value = "genreType") String genreType){
-        Genre genre = genreRepository.getGenreByType(genreType);
+        Genre genre = genreRepository.getGenreByType(genreType.toLowerCase(Locale.ROOT));
         if(genre == null) {
-            Genre newGenre = new Genre(genreType);
+            Genre newGenre = new Genre(genreType.toLowerCase());
             genreRepository.save(newGenre);
             return "Успешно добавен жанр!";
         } else {
